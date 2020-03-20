1f612c0b9abbc42b577502bda4960f4a9124f30a
ChungNPH <hchung1997@gmail.com>
2020-03-20 16:46:41 +0700

    'update'

--- client/client_request.py
+++ client/client_request.py
@@ -5,15 +5,21 @@ import argparse
 def call_api():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--username")
-    parser.add_argument("--password")
-    parser.add_argument("--image")
+    parser.add_argument("-u", "--username", help='<Required> Set flag', required=True)
+    parser.add_argument("-p", "--password", help='<Required> Set flag', required=True)
+    parser.add_argument("-i", "--images", nargs='+', help='<Required> Set flag', required=True)
 
     args = parser.parse_args()
     api = "http://localhost:5000/api/ocr"
-    files = {'image': open('./libs/screenshots/'+args.image, 'rb')}
+    print(args)
+
+    fs = {}
+    for i in range(len(args.images)):
+        fs[str(i)] = open('./libs/screenshots/'+args.images[i], 'rb')
+
+    print(fs)
     params = {"user_name" : args.username, "password" : args.password}
-    print(requests.post(api,params, files=files ).text)
+    print(requests.post(api, params, files=fs).text)
 
 if __name__ == "__main__":
     call_api()
--- server/main_service.py
+++ server/main_service.py
@@ -16,8 +16,10 @@ def upload_file():
     user_name = request.form['user_name']
     password = request.form['password']
     check = False
+
     if request.method == "GET":
         return "This is the api BLah blah"
+
     elif request.method == "POST":
         with open(r'.\libs\accounts.json') as f:
             data = json.load(f)
@@ -25,21 +27,24 @@ def upload_file():
             if user_name == v['user_name'] and password == v['password']:
                 check = True
 
+        result = ''
         if check:
-            file = request.files['image']
-            f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-            file.save(f)
-            # print(file.filename)
-            image = cv2.imread(UPLOAD_FOLDER+"/"+file.filename)
-            os.remove(UPLOAD_FOLDER+"/"+file.filename)
-            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            filename = "{}.png".format(os.getpid())
-            cv2.imwrite(filename, gray)
-            text = handler(filename)
-            os.remove(filename)
+
+            for i in request.files:
+                file = request.files[i]
+                f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+                file.save(f)
+                image = cv2.imread(UPLOAD_FOLDER+"/"+file.filename)
+                os.remove(UPLOAD_FOLDER+"/"+file.filename)
+                gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                filename = "{}.png".format(file.filename)
+                cv2.imwrite(filename, gray)
+                text = handler(filename)
+                result = result + (i+'\n'+text)
+                os.remove(filename)
         else:
-            text = "denied"
-        return jsonify({"text" : text})
+            result = "denied"
+        return jsonify({"text" : result})
 
 app.run("0.0.0.0",5000,threaded=True,debug=True)
 
